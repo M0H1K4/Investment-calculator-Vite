ffd55abd478093354afc3d2c20164c9f552b00ee
M0H1K4N <nejer911@gmail.com>
2023-10-31 15:43:05 +0400

    added styles to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
   return (
-    <header>
+    <header id="header">
       <img  src={logo} />
       <h1>Invenstment Calculator</h1>
     </header>
