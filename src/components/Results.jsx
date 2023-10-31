735005606c9f701f4a1c8443bb01779d251a199e
M0H1K4N <nejer911@gmail.com>
2023-10-31 16:37:54 +0400

    add: lean code

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ input }) {
- const resultData = calculateInvestmentResults(input);
-  return <p>Results...</p>;
+  const resultData = calculateInvestmentResults(input);
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <thbody>
+        {resultData.map(yearData => {
+          return <tr key={yearData.year}>
+            <td>{yearData.year}</td>
+            <td>{formatter.format(yearData.valueEndOfYear)}</td>
+            <td>{formatter.format(yearData.interest)}</td>
+            <td>{}</td>
+            <td>{}</td>
+          </tr>
+        })}
+      </thbody>
+    </table>
+  );
 }
